d4f92655a13e70236ea68563231a6d49485c373f
= <=>
2023-09-30 12:58:42 +0100

    first commit

---
+++ lops.js
@@ -0,0 +1,31 @@
+
+
+
+ 
+   let today = "sunday"
+
+if (today == "saturday") {
+   console.log("come to coding class") }  
+   
+   else if (today =="sunday") {
+   console.log("go sleep")}
+   
+   else if 
+     (today=="monday") {
+      console.log("go william ellis school")
+   } else {
+
+      
+   }
+
+   let today = "monday"
+
+   if (today == monday) {
+      console.log("1")
+      
+   }
+   if (today ==) {
+      
+   } else {
+      
+   }

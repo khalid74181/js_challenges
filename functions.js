a12bab5eadbcdca7813162dce1e67b16f64fd533
= <=>
2023-10-07 13:18:39 +0100

    seperate files

---
+++ functions.js
@@ -0,0 +1,13 @@
+//write a simple javascript function called multiply that takes two numbers as params and returns the caller
+
+
+
+
+
+
+
+function multiple (x , y) {
+    return x * y;
+}
+let results = 4 * 2 ;
+console.log(results)
